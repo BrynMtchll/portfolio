78886e61611f8ba69acd7f62dfa408c32945f49c
BrynMtchll <brynlymitchell@Brynlys-MacBook-Pro.local>
2020-10-21 12:55:23 +1100

    Updates

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
    // handles reloads
    let hash = window.location.hash;
    if (hash == '#inspiration') {
-      $(".current").load('../dist/inspiration.html');
+      $(".current").load('inspiration.html');
 
    } else if (hash == '#landing') {
-      $(".current").load('../dist/landing.html');
+      $(".current").load('landing.html');
    } else if (hash == '#classTasks') {
-      $(".current").load('../dist/classTasks.html');
+      $(".current").load('classTasks.html');
    } else {
-      $(".current").load('../dist/landing.html');
+      $(".current").load('landing.html');
       window.history.replaceState({ page: "inspiration" }, "gd-portfolio-inspiration", "index.html#landing");
 
    }
@@ -19,7 +19,7 @@ $(document).ready(function () {
    window.onhashchange = function () {
       let hash = window.location.hash;
       hash = hash.substring(1);
-      let url = '../dist/' + hash + '.html';
+      let url = '' + hash + '.html';
       $(".current").load(url);
 
    }
@@ -27,11 +27,11 @@ $(document).ready(function () {
    // handles click - doesn't refresh, pushes state
    $('#inspirationItemLink').on('click', function (e) {
       console.log('called');
-      $(".current").load('../dist/inspiration.html');
+      $(".current").load('inspiration.html');
       window.history.pushState({ page: "inspiration" }, "gd-portfolio-inspiration", "#inspiration");
    });
    $('#classTasksItemLink').on('click', function (e) {
-      $(".current").load('../dist/classTasks.html');
+      $(".current").load('classTasks.html');
       window.history.pushState({ page: "classTasks" }, "gd-portfolio-inspiration", "#classTasks");
    });
 
